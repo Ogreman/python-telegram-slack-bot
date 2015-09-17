c1453cd432630b2af2b96d2432cb122619529ea8
James Cox <james@actual-experience.com>
2015-09-17 23:18:10 +0100

    users

--- t2sbot.py
+++ t2sbot.py
@@ -2,20 +2,17 @@ import os
 import logging
 import telegram
 import requests
-
+import time
 
 LAST_UPDATE_ID = None
-USER_NAMES = [
-    val
-    for key, val in os.environ.items()
-    if key.startswith('USER_NAME')
-]
 TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
 SLACK_TOKEN = os.environ['SLACK_TOKEN']
 SLACK_SUB = os.environ['SLACK_SUB']
 SLACK_URL = "https://{slack}.slack.com/services/hooks/slackbot?token={token}&channel=%23{channel}"
 SLACK_CHANNEL = os.environ.get('SLACK_CHANNEL', 'chat')
-OTHER_BOT_URL = "{url}/register".format(url=os.environ['OTHER_BOT_URL'])
+OTHER_BOT_REG = "{url}/register".format(url=os.environ['OTHER_BOT_URL'])
+OTHER_BOT_USR = "{url}/users".format(url=os.environ['OTHER_BOT_URL'])
+USER_NAMES = []
 
 
 def main():
@@ -36,11 +33,18 @@ def main():
 
     while True:
         echo_to_slack(bot)
+        time.sleep(0.4)
 
 
 def echo_to_slack(bot):
     global LAST_UPDATE_ID
     global SLACK_CHANNEL
+    global USER_NAMES
+
+    try:
+        USER_NAMES = requests.get(OTHER_BOT_USR).json().get('users', [])
+    except requests.exceptions.ConnectionError:
+        pass
 
     # Request updates after the last updated_id
     for update in bot.getUpdates(offset=LAST_UPDATE_ID, timeout=10):
@@ -48,15 +52,15 @@ def echo_to_slack(bot):
         message = update.message.text.encode('utf-8')
         user = update.message.from_user.username
 
-        if message and user in USER_NAMES:
-            if message.startswith('/channel'):
+        if message:
+            if message.startswith('/channel') and user in USER_NAMES:
                 SLACK_CHANNEL = message.split(' ')[1].lower()
                 bot.sendMessage(chat_id=chat_id, text="Channel set to %s" % SLACK_CHANNEL)
             elif message.startswith('/register'):
                 user_id = update.message.from_user.id
                 try:
                     response = requests.post(
-                        OTHER_BOT_URL,
+                        OTHER_BOT_REG,
                         data=dict(
                             username=user,
                             id=user_id
@@ -66,7 +70,7 @@ def echo_to_slack(bot):
                         bot.sendMessage(chat_id=chat_id, text="Registered with my pal.")
                 except requests.exceptions.ConnectionError:
                     bot.sendMessage(chat_id=chat_id, text="Something went wrong...")
-            else:
+            elif user in USER_NAMES:
                 try:
                     response = requests.post(
                         SLACK_URL.format(
@@ -84,6 +88,8 @@ def echo_to_slack(bot):
                         bot.sendMessage(chat_id=chat_id, text="Sent!")
                 except requests.exceptions.ConnectionError:
                     bot.sendMessage(chat_id=chat_id, text="Something went wrong...")
+            else:
+                bot.sendMessage(chat_id=chat_id, text="Register first...")
 
             # Updates global offset to get the new updates
             LAST_UPDATE_ID = update.update_id + 1
